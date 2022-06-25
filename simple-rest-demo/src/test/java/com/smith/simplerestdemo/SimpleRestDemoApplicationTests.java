41ca5a9e7a9545c894052d3ae72be7b4e7e46d90
jsmith <jilks.smith@cellulant.io>
2022-06-25 18:50:08 +0300

    Add simple-rest-demo

---
+++ simple-rest-demo/src/test/java/com/smith/simplerestdemo/SimpleRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smith.simplerestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
